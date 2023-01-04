23d1a4a13aae040d3883e0e783feb6f28f3758f9
Krish Sadhwani <92883393+KrishT97@users.noreply.github.com>
2023-01-04 10:26:18 +0000

    Update MatrixMultiplyReduce.java

--- matrix_exercise/src/main/java/MatrixMultiplyReduce.java
+++ matrix_exercise/src/main/java/MatrixMultiplyReduce.java
@@ -6,12 +6,10 @@ public class MatrixMultiplyReduce extends Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        // Sum up the values for each cell in the output matrix
         int sum = 0;
         for (Text value : values) {
             sum += Integer.parseInt(value.toString());
         }
-        // Emit the final value for the cell in the output matrix
         context.write(key, new Text(String.valueOf(sum)));
     }
 
